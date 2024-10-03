975b2ebdf4cd70df22355cfccde26a35ca6108f8
rocktohq <itzmonir@gmail.com>
2024-10-04 01:15:45 +0600

    ErrorPage updated

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,26 @@
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
 
 const ErrorPage = () => {
   return (
-    <div>
+    <>
       <Helmet>
         <title>404</title>
       </Helmet>
-      Error: 404 Not Found
-    </div>
+      <main className="grid min-h-full place-items-center bg-white px-6 py-24 sm:py-32 lg:px-8">
+        <div className="text-center">
+          <p className="text-base font-semibold text-indigo-600">404</p>
+          <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Page not found</h1>
+          <p className="mt-6 text-base leading-7 text-gray-600">{`Sorry, we couldn’t find the page you’re looking for.`}</p>
+          <div className="mt-10 flex items-center justify-center gap-x-6">
+          <Link to={"/"} className="btn btn-primary rounded-none text-white"><span aria-hidden="true">&larr;</span> Go back Home</Link>
+            <Link to={"/contact"} className="text-sm font-semibold text-gray-900">
+              Contact Support <span aria-hidden="true">&rarr;</span>
+            </Link>
+          </div>
+        </div>
+      </main>
+    </>
   );
 };
 
